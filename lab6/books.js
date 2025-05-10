d122414846a38f9dd26ec530192c1a85786ae5c9
Алина Геворкян <aalinkaaaaaaaaaaa@vk.com>
2025-05-10 18:22:13 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 500,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 800,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 1200,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 650,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Рэй Брэдбери',
+        name: '451 градус по Фаренгейту',
+        publicationYear: 1953,
+        price: 700,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Айн Рэнд',
+        name: 'Атлант расправил плечи',
+        publicationYear: 1957,
+        price: 1500,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Франц Кафка',
+        name: 'Процесс',
+        publicationYear: 1925,
+        price: 900,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Габриэль Гарсиа Маркес',
+        name: 'Сто лет одиночества',
+        publicationYear: 1967,
+        price: 1100,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Федор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 850,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: 1300,
+        bestseller: true,
+        available: true
+    }
+];
